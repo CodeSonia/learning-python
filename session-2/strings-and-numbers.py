3b9ec013dafe0b3cf67df495f95a95d2651c833f
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-02-10 14:56:06 +0000

    Session 2

---
+++ session-2/strings-and-numbers.py
@@ -0,0 +1,13 @@
+first_name = 'fred'
+last_name = 'flintstone'
+
+# first_name = input('First name: ')
+# last_name = input('Last name: ')
+
+print(first_name.capitalize() + ' ' + last_name.capitalize())
+print(f'{first_name} {last_name}')
+print('{} {}'.format(first_name, last_name))
+
+first_num = int(input('First num: '))
+second_num = int(input('Second num: '))
+print(f'{first_num} + {second_num}')
