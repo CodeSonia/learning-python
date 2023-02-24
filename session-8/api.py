a15db149e064772a421941f6723afe372263cddd
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-02-24 12:38:39 +0000

    Added an additional API call to dog facts

--- session-8/api.py
+++ session-8/api.py
@@ -1,14 +1,20 @@
 import requests
 
+# Using astros api
 # r = requests.get('http://api.open-notify.org/astros.json')
 # r.status_code
 # print(r.status_code)
 
 # print(r.json())
 
-star_wars = requests.get('http://swapi.dev/api/planets/1/')
-print(star_wars.status_code)
+#Using star wars api
+# star_wars = requests.get('http://swapi.dev/api/planets/1/')
+# print(star_wars.status_code)
 
-print(star_wars.json())
+# print(star_wars.json())
 
+# dogs_facts = requests.get('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all')
+# print(dogs_facts.status_code)
+
+# print(dogs_facts.json())
 
