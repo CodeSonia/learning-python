05b47b922e3215ec234d07d028177204bfc06c68
Rosa Fox <rosa.fox@digital.cabinet-office.gov.uk>
2023-03-10 11:31:48 +0000

    WIP - convert_direction

--- course-1-game/game.py
+++ course-1-game/game.py
@@ -1,5 +1,35 @@
 import requests
 
+def convert_direction(current_node, current_location):
+    nodes = current_node['nodes']
+    can_go = []
+    for node in nodes:
+        x = int(node[0])
+        y = int(node[2])
+
+        current_node_x = int(current_location[0])
+        current_node_y = int(current_location[2])
+
+
+        if x == current_node_x and y > current_node_y:
+            can_go.append("n")
+
+        if x == current_node_x and y < current_node_y:
+            can_go.append("s")
+
+        if x < current_node_x and y == current_node_y:
+            can_go.append("w")
+
+        if x > current_node_x and y == current_node_y:
+            can_go.append("e")
+    print(can_go)
+    return can_go
+
+    #same x but y is higher = north
+    #same x but y is lower = south
+    #x is lower and y is the same = west
+    #x is higher and y is the same = east
+
 r = requests.get('https://7w298.wiremockapi.cloud/labyrinth')
 if r.status_code == 200:
     print(r.status_code)
@@ -8,8 +38,9 @@ if r.status_code == 200:
     print(data['description'])
 
     start_node = ""
-
+    current_location = ""
     for location in data['maze']:
+        current_location = location
         node = data["maze"][location]
         if node['entrance'] == True:
             print("You are in the game!")
@@ -22,12 +53,10 @@ if r.status_code == 200:
 
     while current_node['alive'] and not current_node["exit"]:
         print(current_node['description'])
-        next_node = input(f"Where would you like to move? The available nodes are: {current_node['nodes']}")
+        next_node = input(f"Where would you like to move? The available nodes are: {convert_direction(current_node, current_location)}")
 
         current_node = data['maze'][next_node]
 
     print(current_node['description'])
 else:
     print(f"Sorry, something went wrong! The status code: {r.status_code} ")
-
-
