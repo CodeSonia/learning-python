a3dcf05725c0855868a463850065e5b525302f1f
Rosa Fox <rosafox89@gmail.com>
2023-03-24 14:56:59 +0000

    Encode and decode messages

--- session-17/enigma.py
+++ session-17/enigma.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 
 class Reflector:
@@ -36,14 +37,21 @@ class Machine:
     def encode_char(self, letter):
         for rotor in self.rotor_setting:
             letter = rotor.value_from_key(letter)
-        letter = self.reflector_setting.value_from_key(letter) 
+        letter = self.reflector_setting.value_from_key(letter)
 
         for rotor in reversed(self.rotor_setting):
-            letter = rotor.key_from_value(letter)  
-        return letter 
-    
-     
+            letter = rotor.key_from_value(letter)
+        return letter
 
+    def encode_string(self, message):
+        message = self.sanitise_message(message)
+        encoded_message = []
+        for letter in [*message.upper()]:
+            encoded_message.append(self.encode_char(letter))
+        return "".join(encoded_message)
+
+    def sanitise_message(self, message):
+        return re.sub(r"[^A-Za-z]*", "", message)
 
 mappings = requests.get('https://7w298.wiremockapi.cloud/enigma/mappings/long')
 mapping_data = mappings.json()
@@ -59,7 +67,12 @@ z = Rotor("III")
 machine = Machine()
 machine.config([x, y, z], r)
 
-print(machine.encode_char("H"))
+encode_message = machine.encode_string("wEl ovep  3ythOn!!!!!")
+decode_message = machine.encode_string(encode_message)
+print(encode_message)
+print(decode_message)
+
+# print(machine.encode_char("H"))
 
 
 # print(x.value_from_key("H"))
