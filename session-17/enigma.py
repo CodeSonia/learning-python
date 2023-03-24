d18ee38409638fb663982a7b6af804c8a9a8193b
sonia hussain <sonia.hussain@digital.cabinet-office.gov.uk>
2023-03-24 14:22:06 +0000

    adding more methods to the machine class

--- session-17/enigma.py
+++ session-17/enigma.py
@@ -1,19 +1,20 @@
 import requests
 
+
 class Reflector:
     def __init__(self, id):
         self.id = id
-        pass
 
-    def value_from_key(self, key):  
+    def value_from_key(self, key):
         return self.get_key_setting()[key]
 
     def key_from_value(self, value):
         key = [k for k, v in self.get_key_setting().items() if v == value]
         return key[0]
-        
+
     def get_key_setting(self):
         return mapping_data["reflectors"][self.id]
+
 
 class Rotor(Reflector):
     def __init__(self, id):
@@ -23,30 +24,49 @@ class Rotor(Reflector):
     def get_key_setting(self):
         return mapping_data["rotors"][self.id]
 
+
 class Machine:
     def __init__(self):
-        
-      pass
+        pass
 
+    def config(self, rotor_setting, reflector_setting):
+        self.rotor_setting = rotor_setting
+        self.reflector_setting = reflector_setting
+
+    def encode_char(self, letter):
+        for rotor in self.rotor_setting:
+            letter = rotor.value_from_key(letter)
+        letter = self.reflector_setting.value_from_key(letter) 
+
+        for rotor in reversed(self.rotor_setting):
+            letter = rotor.key_from_value(letter)  
+        return letter 
+    
+     
 
 
 mappings = requests.get('https://7w298.wiremockapi.cloud/enigma/mappings/long')
-
 mapping_data = mappings.json()
 
-rotor_setting = ["I","II","III"]
+rotor_setting = ["I", "II", "III"]
 
 
-r = Reflector("A")   
+r = Reflector("A")
 x = Rotor("I")
 y = Rotor("II")
 z = Rotor("III")
 
-print(x.value_from_key("H"))  
-print(x.key_from_value("Q")) 
-print(y.value_from_key("H"))  
-print(y.key_from_value("Q"))  
-print(z.value_from_key("H"))  
-print(z.key_from_value("Q"))   
-print(r.value_from_key("H"))  
-print(r.key_from_value("Q"))   
+machine = Machine()
+machine.config([x, y, z], r)
+
+print(machine.encode_char("H"))
+
+
+# print(x.value_from_key("H"))
+# print(x.key_from_value("Q"))
+# print(y.value_from_key("H"))
+# print(y.key_from_value("Q"))
+# print(z.value_from_key("H"))
+# print(z.key_from_value("Q"))
+# print(r.value_from_key("H"))
+# print(r.key_from_value("Q"))
